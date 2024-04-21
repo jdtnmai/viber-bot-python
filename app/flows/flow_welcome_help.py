cc4b49438ef577d79550933a0d9b3caa2959d6fa
Justas Kalpokas <justas@datanome.ai>
2024-04-21 17:28:07 +0300

    quick fix to welcome message.

--- app/flows/flow_welcome_help.py
+++ app/flows/flow_welcome_help.py
@@ -8,12 +8,12 @@ def send_welcome_help_message(viber, viber_message: ViberMessage):
     welcome_help_message_text = WELCOME_HELP_MESSAGE
     tracking_data = TrackingData(system_message=True)
 
-    viber_message = MessageBuilder.build_viber_message(
+    response_to_viber_message = MessageBuilder.build_viber_message(
         message_text=welcome_help_message_text,
         tracking_data=asdict(tracking_data),
     )
     MessageSenger.send_viber_messagess(
         viber=viber,
         recipient_viber_id=viber_message.sender_viber_id,
-        viber_message=viber_message,
+        viber_message=response_to_viber_message,
     )
